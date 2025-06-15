d1c7aceb74f6e9d4bc41e2aef2f2179ec1624701
Nikita Michael <nikitabridgette04@gmail.com>
2025-06-15 19:29:10 +0000

    Datatypes

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "nikita@google.com"
+var accountPassword = "12345"
+accountCity = "Hoshiarpur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "1156"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as new version
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log (3 
+    +3) // code readability should be high
+
+console.log ("Nikita")
+
+let name = "Nikita"
+let age = 27
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+//boolean => true/false
+// null (standalone value)
+//undefined
+// symbol =. unique
+
+//object
+
+console.log (typeof "hitesh") //undefined
+console.log (typeof age) // int
+console.log (typeof null) // object
