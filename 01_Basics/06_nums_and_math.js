8694d9c315db21bb7c9a535a56a1ab184093065b
Nikita Michael <nikitabridgette04@gmail.com>
2025-06-17 16:46:39 +0000

    nums&math

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,10 @@
+const score = 400;
+
+const Balance = new Number (100);
+// const name = new String ('Ankit');
+console.log(Balance);
+console.log(typeof Balance);
+console.log(typeof score);
+// console.log(typeof name);
+
+
