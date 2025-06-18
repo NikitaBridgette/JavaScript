c559469f787a7469852f60fd7d27d39c4007d45e
Nikita Michael <nikitabridgette04@gmail.com>
2025-06-18 19:29:13 +0000

    Numbers & Math

--- 01_Basics/06_nums_and_math.js
+++ 01_Basics/06_nums_and_math.js
@@ -2,9 +2,37 @@ const score = 400;
 
 const Balance = new Number (100);
 // const name = new String ('Ankit');
-console.log(Balance);
-console.log(typeof Balance);
-console.log(typeof score);
+// console.log(Balance);
+// console.log(typeof Balance);
+// console.log(typeof score);
 // console.log(typeof name);
 
+// console.log(Balance.toString());
+// // console.log(Balance.toString().length);
+// console.log(Balance.toFixed(2));
 
+const otherNumber= 23.8966
+// console.log(otherNumber.toPrecision(3));
+
+const otherNumber1= 123.8966
+// console.log(otherNumber1.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Math Library +++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+// Random integer between 1 and 6 (like a dice):
+// console.log(Math.floor(Math.random() * 6) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min )
